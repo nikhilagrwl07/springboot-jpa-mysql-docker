25a04591aaac9fb6c31a24a9439445a112433caa
Nikhil Agrawal <nikhil.agrwl07@gmail.com>
2018-04-08 17:34:49 +0530

    adding minikube as one of spring profile

--- stop.sh
+++ stop.sh
@@ -3,5 +3,5 @@ kubectl delete -f scripts/deployment-gym.yml
 kubectl delete -f scripts/service-gym.yml
 kubectl delete -f scripts/ingress-gym.yml
 kubectl delete cm hostname-config
-#kubectl delete -f scripts/mysql.yml
+kubectl delete -f scripts/mysql.yml
 
